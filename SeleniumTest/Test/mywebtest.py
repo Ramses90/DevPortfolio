ad578dbb3019b84869c03a52d80103029cfc43cb
Ramses <machadoramses@yahoo.com>
2019-08-30 05:22:20 -0500

    TestsUpdated

--- SeleniumTest/Test/mywebtest.py
+++ SeleniumTest/Test/mywebtest.py
@@ -1,27 +1,45 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium import webdriver
 import unittest
+import HtmlTestRunner
+
 
 class MywebTest(unittest.TestCase):
-    
+
     @classmethod
-    def setUpClass(cls):        
-        cls.driver = webdriver.Chrome(executable_path='../drivers/chromedriver')
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(
+            executable_path='SeleniumTest/drivers/chromedriver')
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
-    
-    def test_serach(self):
-        self.driver.get("https://selenium-python.readthedocs.io/faq.html")
-        self.driver.find_element_by_name("q").send_keys("Serach for my name")
-        self.driver.find_element_by_name("btnk").click()
+
+    def test_search(self):
+        self.driver.get(
+            "http://ec2-18-218-65-248.us-east-2.compute.amazonaws.com:8000/")
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def test_search3(self):
+        self.driver.get("http://ec2-18-218-65-248.us-east-2.compute.amazonaws.com:8000/")
+        self.driver.find_element_by_link_text("#about").click()
+        self.driver.find_element_by_name("About").click()
+
 
     def test_serach2(self):
         self.driver.get("https://google.com")
-        self.driver.find_element_by_name("q").send_keys("Serach for my name")
-        self.driver.find_element_by_name("btnk").click()
+        self.driver.find_element_by_name("q").send_keys("DevPortFolio")
 
     @classmethod
-    def tearDownClass(cls): 
+    def tearDownClass(cls):
         cls.driver.close()
         cls.driver.quit()
         print("Test Completed")
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(
+        output='SeleniumTest/reports'))
+
+#pip install html-testRunner
+

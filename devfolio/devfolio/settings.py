1b97d5d048dc405d32f30096918d451a92ce4a36
Ramses <machadoramses@yahoo.com>
2019-09-01 08:56:43 -0500

    hosts allowed

--- devfolio/devfolio/settings.py
+++ devfolio/devfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7*l%=$klc-lb226(uiqj#(397u4bf_!2f0f$$h9@01evsk3*&8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition

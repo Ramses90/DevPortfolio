2ac6643a96b0ea2228c559e618c18df5bc808e90
Ramses Machado <machadoramses@yahoo.com>
2019-09-01 18:54:54 +0530

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #chown www-data:www-data /var/www/html/ -R
-python3 /devportfoliodevfolio/manage.py runserver 0.0.0.0:8000
+python3 /devportfolio/devfolio/manage.py runserver 0.0.0.0:8000
